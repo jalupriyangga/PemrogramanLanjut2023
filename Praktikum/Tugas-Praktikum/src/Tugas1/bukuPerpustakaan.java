ce12c28310e1b44ddc1aed025c7c6e8f98bd38ea
jalupriyangga <dionisiusaritojp@gmail.com>
2023-03-10 14:23:25 +0700

    Membuat kerangka / clue pengerjaan tugas dari kakaknya

--- Praktikum/Tugas-Praktikum/src/Tugas1/bukuPerpustakaan.java
+++ Praktikum/Tugas-Praktikum/src/Tugas1/bukuPerpustakaan.java
@@ -1,4 +1,12 @@
 package Tugas1;
 
+class daftarbuku{
+    // rekomendasinya menggunakan setter getter
+    // boleh buat 7 class
+    // boleh melalui method saja, menggunakan array string
+}
 public class bukuPerpustakaan {
+    public static void main(String[] args) {
+
+    }
 }
