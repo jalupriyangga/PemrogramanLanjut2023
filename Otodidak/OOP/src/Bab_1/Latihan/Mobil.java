ccbb964bac4a66942cbf25162bb7d2d5da950644
jalupriyangga <dionisiusaritojp@gmail.com>
2023-03-10 14:00:42 +0700

    -

--- Otodidak/OOP/src/Bab_1/Latihan/Mobil.java
+++
@@ -1,9 +0,0 @@
-package Bab_1.Latihan;
-/* Author info
-   Name : Dionisius Arito Jalu Priyangga
-   NIM  : 225150701111032
-*/
-public class Mobil {
-    String warna;
-    int tahunProduksi;
-}
--- Otodidak/OOP/src/Bab_2/Latihan/MobilKonstruktor.java
+++
@@ -1,8 +0,0 @@
-package Bab_2.Latihan;
-
-public class MobilKonstruktor {
-    public static void main(String[] args) {
-        Mobil mobilku = new Mobil("Merah", 2003);
-        mobilku.info();
-    }
-}
